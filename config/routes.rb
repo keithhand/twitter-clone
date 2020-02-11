4a5613e8f1da9819d6fabb8266d98a02a0e7fd4b
Keith Hand <keith@clevyr.com>
2020-02-10 20:17:34 -0600

    added relationships between users

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,15 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   get    '/signup',  to: 'users#new'
   post   '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -3,11 +3,14 @@ require 'test_helper'
 class UsersProfileTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "profile display" do
+    log_in_as(@user)
     get user_path(@user)
     assert_template 'users/show'
+    assert_select 'div[id=follow_form]', count: 0
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
@@ -16,5 +19,19 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+  end
+
+  test "profile display for unfollowed user" do
+    log_in_as(@user)
+    @user.unfollow(@other_user)
+    get user_path(@other_user)
+    assert_select 'div[id=follow_form]>form>input[type=submit][value=?]', "Follow"
+  end
+
+  test "profile display for followed user" do
+    log_in_as(@user)
+    @user.follow(@other_user)
+    get user_path(@other_user)
+    assert_select 'div[id=follow_form]>form>input[type=submit][value=?]', "Unfollow"
   end
 end
